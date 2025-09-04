873a2f4c46ad0061e3655c417e97919161c215da
Anika Lohia <anikalohia2@gmail.com>
2025-09-04 19:26:33 +0530

    scroll error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+
 import Hero from "./Components/hero";
 import About from "./Components/about";
 import Project from "./Components/project";
 import Contact from "./Components/contact";
-import { Canvas } from "@react-three/fiber";
 import Navbar from "./Components/navbar"; 
 import "./style.css";
-import Ballpit from "./Ballpit/Ballpit";
 import Skill from "./Components/skill";
 
 export default function LandingPage() {
